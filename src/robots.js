ac58ca61611b029cf51f4fbef61300b71ecd32f8
Dennis Weber <dennisweber@denniss-mbp.speedport_w_724v_typ_a_05011603_06_003>
2020-12-23 09:15:12 +0100

    uplaod files

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Dennis Weber",
+        username: "DW-Scientist",
+        email: "d7weber@live.de"
+    },
+    {
+        id: 2,
+        name: "Carmen Ambros",
+        username: "Ambrosito",
+        email: "ca@web.com"
+    },
+    {
+        id: 3,
+        name: "Valentin Weber",
+        username: "BlackMamba",
+        email: "vw@web.com"
+    },
+    {
+        id: 4,
+        name: "Joshua Woelfle",
+        username: "AskingForHair",
+        email: "jw@web.com"
+    },
+    {
+        id: 5,
+        name: "Wolfgang Lang",
+        username: "DoggyStyle",
+        email: "wl@web.com"
+    },
+    {
+        id: 6,
+        name: "Marco Waegele",
+        username: "BallsToGround",
+        email: "mw@web.com"
+    },
+    {
+        id: 7,
+        name: "Timo Bunk",
+        username: "TechWithTimo",
+        email: "tb@web.com"
+    },
+    {
+        id: 8,
+        name: "Matthias Maier",
+        username: "MolecularRockstar",
+        email: "mm@web.com"
+    },
+    {
+        id: 9,
+        name: "Tobias Grauer",
+        username: "Obdachloser",
+        email: "tg@web.com"
+    }
+]
